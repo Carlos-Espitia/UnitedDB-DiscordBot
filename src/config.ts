5b0390ff1a33ba28c59dfa726451f907fe658df0
XGNFox <94414099+XGNFox@users.noreply.github.com>
2022-06-15 21:01:38 -0400

    Update config.ts
    add me to staff

--- src/config.ts
+++ src/config.ts
@@ -15,11 +15,13 @@ export const config = {
         '943970209075707985', //Lantern#7563
         '578611714988441634', //Lego#9666
         '857334147680305203', //⃤🌌Mx.Bread🌌⃤
+        `815033074378342460`, //Simply
     ],
     Admins: [
         '421053214230708225', //carlos
         '650442844078735360', //love
         '566770844286844953', //beluga
+        `815033074378342460`, //Simply
     ],
     UnitedDBLoginAdmin: 'UnitedDBadmin:sawe1459',
     UnitedDBLoginStaff: 'UnitedDBstaff:sawe1459',
